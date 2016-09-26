532b77009bb2bd1d6934381d0989d6f0b59eb5a0
Peh4 <peha@free.fr>
2016-09-26 16:16:51 +0200

    bower config

--- brunch-config.js
+++ brunch-config.js
@@ -17,9 +17,8 @@ module.exports = {
 				, includePaths: [
           	'node_modules/bourbon/core'
           , 'node_modules/bourbon-neat/app/assets/stylesheets'
-          ,	'bower_components/KNACSS/sass'
-          , 'bower_components/font-awesome/scss'
-        	]
+          ,	'node_modules/normalize.css'
+          ]
       	// Set the precision for arithmetic operations.
       	,	precision: 8
       	// To enable embedded source maps, pass the option `sourceMapEmbed`. This is only supported in _native_ mode; Ruby Sass isn't supported.
