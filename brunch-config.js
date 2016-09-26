55fcd39b4eb7cf3ca8b21eda0900f8344e7f8b7f
Peh4 <peha@free.fr>
2016-09-26 17:06:43 +0200

    add fonts / and knacss

--- brunch-config.js
+++ brunch-config.js
@@ -14,11 +14,6 @@ module.exports = {
 		sass: {
 			options: {
 					mode: 'native'
-				, includePaths: [
-          	'node_modules/bourbon/core'
-          , 'node_modules/bourbon-neat/app/assets/stylesheets'
-          ,	'node_modules/normalize.css'
-          ]
       	// Set the precision for arithmetic operations.
       	,	precision: 8
       	// To enable embedded source maps, pass the option `sourceMapEmbed`. This is only supported in _native_ mode; Ruby Sass isn't supported.
